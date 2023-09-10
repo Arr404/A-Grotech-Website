c62d26a876d424ef06a836cbe2626ecfcbddb8cf
Arr404 <agilfuad@gmail.com>
2023-09-11 00:20:18 +0700

    [V1 ]

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: 'dist',
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+}
+
+module.exports = nextConfig
